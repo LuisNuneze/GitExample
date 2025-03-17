bb3eda951d44b14c15f25402e1ba968119fac59d
LuisNuneze <lnuneze@uninorte.edu.co>
2025-03-17 15:48:59 -0500

    feature2 commit

---
+++ src/main/core/Cat.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.core;
+
+/**
+ *
+ * @author Luis Ernesto Nuñez
+ */
+public class Cat {
+    int age, peso, color;
+    String raza;
+
+    public Cat(int age, int peso, int color, String raza) {
+        this.age = age;
+        this.peso = peso;
+        this.color = color;
+        this.raza = raza;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" + "age=" + age + ", peso=" + peso + ", color=" + color + ", raza=" + raza + '}';
+    }
+}
